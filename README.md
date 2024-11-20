74330e41e4dd5304927a00b5b0436a36447f3790
Cyril Li <cyrils.li@icloud.com>
2024-11-19 20:42:09 -0600

    add pinyin

--- README.md
+++ README.md
@@ -1,30 +1,66 @@
-# Diacritik
+# Diacritik - Input framework-independent pinyin IME and diacritic input for Wayland
 
-Diacritik is a simple Python Tkinter application which provides macOS-style diacritic entry for Linux + Wayland. To insert a character, first type the base letter, then choose one of the options presented using a number key.
+Diacritik is a simple Python Tkinter application which provides macOS-style diacritic entry and Google Pinyin-powered pinyin input for Linux + Wayland. It is designed to be able to work without input frameworks (e.g. `IBus`, `fcitx`) and can be used with any WM/DE with most applications.
 
 ## Dependencies
 
 - Python 3
-- Tkinter
+- tkinter
+- requests (only for pinyin)
 - wtype
 
 ## Installation
 
 Bind a key combination to the `diacritik.py` script. If you are using a tiling window manager, also set windows with a title of `Diacritik` to floating.
 
-## Keymap
-
-The keymap is stored as an object in `keymap.json`. The default keymap is copied from macOS, but can be customized to your liking. The keys of the object are single characters representing the base letter, and the values are strings of possible options with a maximum length of 10. Options will be shown from left to right.
-
 ## Inserting method
 
 Due to complications in (X)Wayland, different applications may require different methods of inserting characters. Currently, the following methods are supported:
-- `hex` - Insert the character by simulating hex code input using Ctrl+Shift+U.
-- `char` - Directly types the character.
+- `hex` - Insert the character(s) by simulating hex code input using Ctrl+Shift+U.
+- `char` - Directly types the character(s).
 
-**Both methods require `wtype` to be installed.**
+**Both methods require `wtype` to be installed. Focus on the desired input field before using the script.**
 
 The script includes a function which checks for the focused application and selects the appropriate method. Configuration is stored in `methods.json`, which includes the following keys:
 - `__default__` - The default method to use.
 - `__cmd__` - A shell command to find the app ID of the focused window.
 - A list of app IDs with the method to use for each.
+
+## Usage
+
+At any point,
+- Press either `control` keys to switch between user and pinyin modes.
+- Press `escape` (or kill the window) to exit the script without typing anything.
+
+## User mode
+
+This mode associates base letters with a set of other characters. It is useful for typing diacritics, but can be used for any other characters as well.
+
+### Keymap
+
+The keymap is stored as an object in `keymap.json`. The default keymap is inspired by macOS, but can be customized to your liking. The keys of the object are single characters representing the base letter (an ASCII character), and the values are strings of possible options (any Unicode characters, max length 10). Options will be shown from left to right, selected using the number keys.
+
+### User flow
+
+- Enter the base letter, using `shift` if necessary.
+- If the base letter is defined in the keymap:
+  - The available options will be displayed.
+  - Press the number key corresponding to the desired option, **or**
+  - Press `enter` to type the base letter, **or**
+  - Press any other key to switch to that base letter.
+- Else, an error message will be displayed. Retry with a different base letter.
+
+## Pinyin mode
+
+This mode uses the Google Pinyin endpoint to input (currently only Simplified Chinese) pinyin, which is then typed out the same way as in user mode. The requests are cached in memory to reduce latency and cleared when the script exits.
+
+### User flow
+
+- Enter a letter in the alphabet.
+- The chacter options (max length 9) will be displayed, with the 10th space representing the current page number.
+- Do one of the following, or keep typing letters to match longer pinyin sequences:
+  - Press the `down` or `right` arrow keys to go to the next page, **or**
+  - Press the `up` or `left` arrow keys to go to the previous page, **or**
+  - Press the number key (1-9) corresponding to a match, which adds the matched character(s) to a buffer, **or**
+  - Press `enter` to type out the buffer along with any unmatched letters, **or**
+  - Press `backspace` to remove unmatched letters (or the buffer if there are no unmatched letters)
--- diacritik.py
+++ diacritik.py
@@ -1,4 +1,30 @@
-import json, os, subprocess, tkinter as tk
+import json, os, subprocess, sys, time, tkinter as tk
+global mode
+
+# Ensure only one instance is running
+def excepthook (exc_type, exc_value, exc_traceback):
+    try:
+        os.rmdir ("/tmp/diacritik/running")
+    except FileNotFoundError:
+        print ("Cannot remove lock file")
+    sys.__excepthook__ (exc_type, exc_value, exc_traceback)
+sys.excepthook = excepthook
+
+try:
+    os.makedirs ("/tmp/diacritik/running")
+except FileExistsError:
+    raise SystemExit ("Duplicate instance")
+
+if not os.path.exists ("/tmp/diacritik/mode"):
+    with open ("/tmp/diacritik/mode", "w") as f:
+        f.write ("user")
+    mode = "user" # user or pinyin
+else:
+    with open ("/tmp/diacritik/mode") as f:
+        mode = f.read ().strip ()
+
+mode_text = lambda: {"user": "Type a key:", "pinyin": "Type pinyin:"} [mode]
+mode_font = lambda: {"user": ("Noto Sans Mono", 32), "pinyin": ("Noto Sans Mono", 32)} [mode] # CHANGE FONT AND SIZE HERE
 
 with open (os.path.join (os.path.dirname (os.path.abspath (__file__)), "keymap.json")) as f:
     key_map = json.load (f)
@@ -9,35 +35,61 @@ app = tk.Tk ()
 app.title ("Diacritik")
 app.configure (padx = 10, pady = 10)
 
-key_label = tk.Label (app, text = "Type a key:", font = ("Courier", 32), fg = "black")
+key_label = tk.Label (app)
+opt_label = tk.Label (app)
+
+pys = {} # Pinyin State
+def setup ():
+    global pys, selecting, app
+    selecting = False
+    pys.update ({ # Pinyin State
+        "page": 1,
+        "options": [], # [[char, length], ...]
+        "matched": "",
+        "unmatched": "",
+        "cache": pys.get ("cache", {}) # Preserve cache across mode switches
+    })
+    key_label.config (text = mode_text (), fg = "black", font = mode_font ())
+    opt_label.config (text = "[ ]" * 10, fg = "black", font = mode_font ())
+
+setup ()
 key_label.pack ()
-opt_label = tk.Label (app, text = "[ ]" * 10, font = ("Courier", 32), fg = "black")
 opt_label.pack ()
 
-selecting = False
-def display_key (event):
+def req_pinyin ():
+    import requests as r
+    global pys, app
+
+    if not pys ["unmatched"]:
+        pys ["page"] = 1
+        return [[" ", 0] for i in range (9)]
+
+    offset = pys ["page"] * 9
+    if pys ["unmatched"] not in pys ["cache"] or len (pys ["cache"] [pys ["unmatched"]]) < offset:
+        try:
+            res = r.get ("https://inputtools.google.com/request?text={}&itc=zh-t-i0-pinyin&num={}&ie=utf-8&oe=utf-8".format (
+                pys ["unmatched"], str (offset)
+            )).json ()
+            if res [0] != "SUCCESS":
+                raise Exception
+        except:
+            key_label.config (text = "Failed to fetch pinyin", fg = "red")
+            return
+
+        offset = min (offset, len (res [1] [0] [1]))
+        cache = [[res [1] [0] [1] [i], res [1] [0] [3].get ("matched_length", [len (pys ["unmatched"])] * offset) [i]] for i in range (offset)]
+        cache.extend ([[" ", 0] for i in range (8 - (len (cache) - 1))]) # Pad to 9
+        offset = len (cache)
+        pys ["page"] = -(-offset // 9)
+        pys ["cache"] [pys ["unmatched"]] = cache
+
+    return pys ["cache"] [pys ["unmatched"]] [offset - 9 : offset]
+
+def key_user (event, key):
     global selecting, key_map, app
-    key = event.char
-    if not key:
-        return
 
-    elif ord (key) == 27: # Escape
-        if selecting:
-            selecting = False
-            opt_label.config (text = "[ ]" * 10)
-            key_label.config (text = "Type a key:", fg = "black")
-        else:
-            app.destroy ()
-        return
-    elif ord (key) == 13: # Enter
-        if selecting:
-            app.destroy ()
-        return
-    elif ord (key) < 32 or ord (key) > 126:
-        return # Ignore non-printable characters
-
-    if selecting and event.char in "1234567890" [ : len (key_map [selecting])]:
-        selecting = key_map [selecting] ["1234567890".index (event.char)]
+    if selecting and key in "1234567890" [ : len (key_map [selecting])]:
+        selecting = key_map [selecting] ["1234567890".index (key)]
         app.destroy ()
         return
     elif selecting:
@@ -51,9 +103,74 @@ def display_key (event):
     else:
         key_label.config (text = f"{key} not found", fg = "red")
 
+def key_pinyin (event, key):
+    global selecting, pys, app
+
+    if key in ("Up", "Left"):
+        if len (pys ["options"]) > 1 and pys ["page"] > 1:
+            pys ["page"] -= 1
+        else:
+            return
+    elif key in ("Down", "Right"):
+        if len (pys ["options"]) > 1:
+            pys ["page"] += 1
+        else:
+            return
+    else:
+        pys ["page"] = 1
+        if key == "BackSpace":
+            if pys ["unmatched"]:
+                pys ["unmatched"] = pys ["unmatched"] [ : -1]
+            elif pys ["matched"]:
+                pys ["matched"] = pys ["matched"] [ : -1]
+            else:
+                return
+        elif key in "abcdefghijklmnopqrstuvwxyz":
+            pys ["unmatched"] += key
+        elif key in "123456789":
+            key = int (key) - 1
+            if len (pys ["options"]) > key + 1 and pys ["options"] [key] [1] > 0:
+                pys ["matched"] += pys ["options"] [key] [0]
+                pys ["unmatched"] = pys ["unmatched"] [pys ["options"] [key] [1] : ]
+        else:
+            return
+
+    pys ["options"] = req_pinyin ()
+    if pys ["options"]:
+        selecting = pys ["matched"] + pys ["unmatched"]
+        key_label.config (text = selecting, fg = "black")
+        opt_label.config (text = "[" + "][".join ([i [0] for i in pys ["options"]] + [str (pys ["page"] if pys ["options"] [0] [0].strip () else " ")]) + "]")
+
+def display_key (event):
+    global first_key, mode, app, selecting
+
+    if event.keysym in ("Control_L", "Control_R"):
+        mode = {"user": "pinyin", "pinyin": "user"} [mode]
+        setup ()
+        return
+    elif mode == "pinyin" and event.keysym in ("Up", "Down", "Left", "Right", "BackSpace"): # Passthrough for pinyin
+        key_pinyin (event, event.keysym)
+        return
+    first_key = False
+
+    key = event.char
+    if not key:
+        return
+    elif ord (key) in (13, 27): # Enter or Escape
+        if ord (key) == 27:
+            selecting = False
+        app.destroy ()
+        return
+    elif ord (key) < 32 or ord (key) > 126:
+        return # Ignore non-printable characters
+    {"user": key_user, "pinyin": key_pinyin} [mode] (event, key)
+
 app.bind ("<Key>", display_key)
 app.mainloop ()
 
+with open ("/tmp/diacritik/mode", "w") as f:
+    f.write (mode)
+os.rmdir ("/tmp/diacritik/running")
 if not selecting:
     raise SystemExit
 
@@ -62,7 +179,9 @@ focused.check_returncode ()
 focused = focused.stdout.decode ().strip ()
 method = methods.get (focused, methods ["__default__"])
 
-if method == "char":
-    os.system (f"wtype {selecting}")
-elif method == "hex":
-    subprocess.run (f'wtype -M ctrl -M shift -k U -m ctrl -m shift {" -k ".join (hex (ord (selecting)) [2 : ])} -k Return'.split ()).check_returncode ()
+for i in selecting:
+    if method == "char":
+        os.system (f"wtype {i}")
+    elif method == "hex":
+        subprocess.run (f'wtype -M ctrl -M shift -k U -m ctrl -m shift {" -k ".join (hex (ord (i)) [2 : ])} -k Return'.split ()).check_returncode ()
+    time.sleep (0.05)
